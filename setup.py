21cf1277757f68a1e35010196281ed44e409c2e0
Forrest Collman <forrest.collman@gmail.com>
2017-12-05 12:18:41 -0800

    fixed setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from setuptools import setup, find_packages
 with open('requirements.txt', 'r') as f:
     required = f.read().splitlines()
 
-setup(name='argschema',
+setup(name='at_synapse_detection',
       version='0.1.0',
       description='synapse analysis for array tomography',
       author='Anish Simhal',
       author_email='anish.simhal@duke.edu',
       packages=find_packages(),
       url='https://github.com/aksimhal/SynapseAnalysis',
-      install_requires=required,
-      dependancy_links=["git+ssh://git@github.com/fcollman/render-python-apps.git@develop"])
+      install_requires=required)
 
