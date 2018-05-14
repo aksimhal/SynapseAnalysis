6169f310feca5d6cbd4b0451c444aae7622a08fa
Anish <aksimhal@gmail.com>
2018-05-14 14:42:34 -0400

    parallel test v1

--- at_synapse_detection/SynapseAnalysis.py
+++ at_synapse_detection/SynapseAnalysis.py
@@ -5,25 +5,28 @@ import pandas as pd
 from skimage import measure
 from at_synapse_detection import SynapseDetection as syn
 from at_synapse_detection import dataAccess as da
-from at_synapse_detection import antibodyAnalysis as aa 
+from at_synapse_detection import antibodyAnalysis as aa
 
-class SynapseAnalysis: 
+
+class SynapseAnalysis:
     """
     Measurements necessary for comparing synapse populations 
     """
+
     def __init__(self, query):
         """
         """
-        self.synapse_density = 0.0 
-        self.num_synapses = 0 
-        self.volume_um3 = 0.0 
-        self.query = query 
+        self.synapse_density = 0.0
+        self.num_synapses = 0
+        self.volume_um3 = 0.0
+        self.query = query
 
-def compute_measurements(resultvol, query, volume_um3, thresh): 
+
+def compute_measurements(resultvol, query, volume_um3, thresh):
     """
     """
-    queryresult = SynapseAnalysis(query) 
-    queryresult.volume_um3 = volume_um3 
+    queryresult = SynapseAnalysis(query)
+    queryresult.volume_um3 = volume_um3
 
     label_vol = measure.label(resultvol > thresh)
     stats = measure.regionprops(label_vol)
@@ -31,6 +34,7 @@ def compute_measurements(resultvol, query, volume_um3, thresh):
     queryresult.synapse_count = len(stats)
 
     return queryresult
+
 
 def create_synapse_df(result_list, row_labels):
     """
@@ -48,14 +52,12 @@ def create_synapse_df(result_list, row_labels):
 
     for n, queryresult in enumerate(result_list):
         df.iloc[n, 0] = queryresult.synapse_density
-        df.iloc[n, 1] = queryresult.synapse_count
-
+        df.iloc[n, 1] = queryresult.num_synapses
 
     return df
 
 
-
-def run_synapses(query_fn, data_location_base, outputFoldername): 
+def run_synapses_astro(query_fn, data_location_base, outputFoldername):
     """
     """
 
@@ -66,7 +68,7 @@ def run_synapses(query_fn, data_location_base, outputFoldername):
 
     foldernames = []
     result_list = []
-    for region_num in range(0, 4): 
+    for region_num in range(0, 4):
         region_name = region_name_base + str(region_num)
         data_location = os.path.join(data_location_base, region_name)
 
@@ -76,57 +78,21 @@ def run_synapses(query_fn, data_location_base, outputFoldername):
             foldernames.append(foldername)
             print(foldername)
             # Load the data
-            
-            synaptic_volumes = da.load_tiff_from_query(query, data_location)
-            volume_um3 = aa.getdatavolume(synaptic_volumes, resolution)
 
-            # Run Synapse Detection
-            resultvol = syn.getSynapseDetections(synaptic_volumes, query)
-
-            # Save the probability map to file, if you want
-            outputNPYlocation = os.path.join(data_location_base, outputFoldername, region_name)
-            syn.saveresultvol(resultvol, outputNPYlocation, 'query_', n)
-
-            queryresult = compute_measurements(resultvol, query, volume_um3, thresh)
-            result_list.append(queryresult)
-
-    df = create_synapse_df(result_list, foldernames)
-    print(df)
-    return df
-
-def run_synapses_astro(query_fn, data_location_base, outputFoldername): 
-    """
-    """
-
-    listOfQueries = syn.loadQueriesJSON(query_fn)
-    resolution = {'res_xy_nm': 100, 'res_z_nm': 70}
-    region_name_base = 'F00'
-    thresh = 0.9
-
-    foldernames = []
-    result_list = []
-    for region_num in range(0, 4): 
-        region_name = region_name_base + str(region_num)
-        data_location = os.path.join(data_location_base, region_name)
-
-        for n, query in enumerate(listOfQueries):
-            print(query)
-            foldername = region_name + '-Q' + str(n)
-            foldernames.append(foldername)
-            print(foldername)
-            # Load the data
-            
-            synaptic_volumes = da.load_tiff_from_astro_query(query, data_location)
+            synaptic_volumes = da.load_tiff_from_astro_query(
+                query, data_location)
             volume_um3 = aa.getdatavolume(synaptic_volumes, resolution)
 
             # Run Synapse Detection
             resultvol = syn.getSynapseDetections_astro(synaptic_volumes, query)
 
             # Save the probability map to file, if you want
-            outputNPYlocation = os.path.join(data_location_base, outputFoldername, region_name)
+            outputNPYlocation = os.path.join(
+                data_location_base, outputFoldername, region_name)
             syn.saveresultvol(resultvol, outputNPYlocation, 'query_', n)
 
-            queryresult = compute_measurements(resultvol, query, volume_um3, thresh)
+            queryresult = compute_measurements(
+                resultvol, query, volume_um3, thresh)
             result_list.append(queryresult)
 
     df = create_synapse_df(result_list, foldernames)
--- data/fragile_x/run_fragX.py
+++ data/fragile_x/run_fragX.py
@@ -9,6 +9,66 @@ from at_synapse_detection import SynapseDetection as syn
 from at_synapse_detection import antibodyAnalysis as aa
 from at_synapse_detection import SynapseAnalysis as sa
 import socket
+import multiprocessing as mp
+import copy
+import numpy as np
+
+
+def run_synapse_detection(query, queryID, nQuery, resolution, data_location, data_location_base,
+                          output_foldername, region_name):
+    """
+    Run synapse detection and result evalution.  The parameters need to be rethought 
+
+    Parameters
+    -------------------
+    query : dict
+    resolution : dict 
+    data_location : str - raw IF data folder (ie F000, F001, F002, F003)
+    data_location_base - folder for the mouse (ie 2ss, 3ss)
+    output_foldername : str (ie result)
+    region_name : str (ie F001)
+    output : Queue object() 
+    """
+
+    print(query)
+
+    # Load the data
+    synaptic_volumes = da.load_tiff_from_query(query, data_location)
+    volume_um3 = aa.getdatavolume(synaptic_volumes, resolution)
+
+    # # Run Synapse Detection
+    # resultvol = syn.getSynapseDetections(synaptic_volumes, query)
+
+    # # Save the probability map to file, if you want
+    # outputNPYlocation = os.path.join(
+    #     data_location_base, output_foldername, region_name)
+    # syn.saveresultvol(resultvol, outputNPYlocation, 'query_', queryID)
+
+    # thresh = 0.9
+    # queryresult = sa.compute_measurements(
+    #     resultvol, query, volume_um3, thresh)
+    queryresult = sa.SynapseAnalysis(query)
+
+    output_dict = {'queryID': queryID,
+                   'query': query, 'queryresult': queryresult}
+    return output_dict
+
+
+def organize_result_lists(result_list):
+    """
+    output_dict = {'queryID': queryID,
+                   'query': query, 'queryresult': queryresult}
+    """
+    query_inds = []
+    list_of_queryresult = []
+    for result in result_list:
+        query_inds.append(result['queryID'])
+        list_of_queryresult.append(result['queryresult'])
+
+    sorted_inds = np.argsort(query_inds)
+    sorted_queryresult = [list_of_queryresult[n] for n in sorted_inds]
+
+    return sorted_queryresult
 
 
 def main():
@@ -24,28 +84,78 @@ def main():
         print('we have arguments')
         print(sys.argv)
 
-
     mouse_number = sys.argv[1]
     mouse_project_str = sys.argv[2]
     sheet_name = sys.argv[3]
 
-    outputFoldername = 'results_' + sheet_name 
+    # mouse_number = 2
+    # mouse_project_str = '2ss'
+    # sheet_name = '2ss_fragX'
+
+    output_foldername = 'results_' + sheet_name
     query_fn = mouse_project_str + '_queries.json'
-    datalocation = '/Users/anish/Documents/yi_mice/' + str(mouse_number) + 'ss_stacks/'
+    datalocation = '/Users/anish/Documents/yi_mice/' + \
+        str(mouse_number) + 'ss_stacks/'
+    data_location_base = '/Users/anish/Documents/yi_mice/' + \
+        str(mouse_number) + 'ss_stacks/'
+
     hostname = socket.gethostname()
     if hostname == 'Galicia':
         datalocation = '/data5TB/yi_mice/' + str(mouse_number) + 'ss_stacks'
+        data_location_base = '/data5TB/yi_mice/'
     print('Query Filename: ', query_fn)
     print('Data Location: ', datalocation)
-    print('OutputFoldername: ', outputFoldername)
+    print('OutputFoldername: ', output_foldername)
     print('Sheetname: ', sheet_name)
-    mouse_df = sa.run_synapses(query_fn, datalocation, outputFoldername)
 
-    #sheet_name = 'FragileX Mouse'
+    listOfQueries = syn.loadQueriesJSON(query_fn)
+    resolution = {'res_xy_nm': 100, 'res_z_nm': 70}
+    region_name_base = 'F00'
+    thresh = 0.9
+
+    result_list = []
+    num_workers = mp.cpu_count() - 1
+    print(num_workers)
+    pool = mp.Pool(num_workers)
+
+    def log_result(result):
+        # This is called whenever foo_pool(i) returns a result.
+        # result_list is modified only by the main process, not the pool workers.
+        result_list.append(result)
+
+    # Setup a list of processes that we want to run
+    processes = []
+    queryID = 0
+    foldernames = []
+    for region_num in range(0, 4):
+        region_name = region_name_base + str(region_num)
+        data_location = os.path.join(data_location_base, region_name)
+
+        for nQuery, query in enumerate(listOfQueries):
+            foldername = region_name + '-Q' + str(nQuery)
+            foldernames.append(foldername)
+            print(foldername)
+
+            pool.apply_async(run_synapse_detection, args=(query, queryID, nQuery, resolution, data_location,
+                                                          data_location_base, output_foldername, region_name), callback=log_result)
+
+            queryID = queryID + 1
+
+    # Run processes
+    pool.close()
+    pool.join()
+
+    print('Get process results from the output queue')
+
+    sorted_queryresult = organize_result_lists(result_list)
+
+    mouse_df = sa.create_synapse_df(sorted_queryresult, foldernames)
+    print(mouse_df)
+
+    sheet_name = 'FragileX Mouse'
     fn = sheet_name + '.xlsx'
     df_list = [mouse_df]
     aa.write_dfs_to_excel(df_list, sheet_name, fn)
-
 
 
 if __name__ == '__main__':
