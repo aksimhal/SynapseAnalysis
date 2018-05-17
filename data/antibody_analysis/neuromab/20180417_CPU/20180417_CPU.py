59ecbc8e0db8a432e9d0f258eecdc4b6fd69f07b
jsterry21 <38295150+jsterry21@users.noreply.github.com>
2018-05-17 10:26:19 -0700

    Striatum validation protocol

---
+++ data/antibody_analysis/neuromab/20180417_CPU/20180417_CPU.py
@@ -0,0 +1,251 @@
+"""
+Run antibody characterization tool on the L110-CCK dataset
+Output an excel sheet of results
+"""
+import os
+import numpy as np
+import pandas as pd #also requires xlsxwriter
+from at_synapse_detection import dataAccess as da
+from at_synapse_detection import antibodyAnalysis as aa
+
+
+
+def CPU():
+    """ Run antibody characterization tool on the L110-CCK dataset
+
+    Return
+    ----------
+    df : dataframe - contains results
+    """
+
+    # Location of data
+    base_dir = "C:/Users/stjst/Desktop/Work/Image_Processing/20180417_Striatum_Validation/raw_align_tiff" #Location of align tif"
+    resolution = {'res_xy_nm': 100, 'res_z_nm': 70} #Resolution of a pixel
+    thresh = 0.9 #What qualifies for final probability map
+    number_of_datasets = 19 #Number of wells
+
+    #Rb Antibody
+    conjugate_fn_str = 'GAD2' #String segment to search in a filename 
+    #Ms Antibody project name, no parent or subclone number needed
+    target_fn_str = 'L106'
+    #Takes base directory string and gives you an array of all the files within
+    filenames = aa.getListOfFolders(base_dir)
+    #
+    conjugate_filenames = []
+    target_filenames = []
+    query_list = []
+    folder_names = []
+
+    for n in range(1, 4):
+        #Use if dataset missing
+        #if n == 21: # Dataset 21 does not exist
+            #continue
+
+        print('Well: ', str(n))
+        folder_names.append('Test-' + str(n)) # Collate 'dataset' names for excel sheet
+        conjugate_str = str(n) + '-' + conjugate_fn_str #creates filename to search for #Creates n-conjugatename
+        target_str = str(n) + '-' + target_fn_str
+
+        # Search for file associated with the specific dataset number
+        indices = [i for i, s in enumerate(filenames) if conjugate_str == s[0:len(conjugate_str)]]
+        conjugate_name = filenames[indices[0]]
+        print(conjugate_name)
+        indices = [i for i, s in enumerate(filenames) if target_str == s[0:len(target_str)]]
+        target_name = filenames[indices[0]]
+        print(target_name)
+        
+        conjugate_filenames.append(conjugate_name)
+        target_filenames.append(target_name)
+
+        # Create query
+        #
+        query = {'preIF': [conjugate_name], 'preIF_z': [2],
+                'postIF': [target_name], 'postIF_z': [1],
+                'punctumSize': 2}
+
+        query_list.append(query)
+
+    n = 4
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'SYNAPSIN' #String segment to search in a filename
+    target_fn_str = 'L109'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [target_name,conjugate_name], 'preIF_z': [1,2], 'postIF': [], 'postIF_z': [], 'punctumSize': 2}
+    query_list.append(query)
+
+    n = 5
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'SYNAPSIN' #String segment to search in a filename
+    target_fn_str = 'L109'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [target_name,conjugate_name], 'preIF_z': [1,2], 'postIF': [], 'postIF_z': [], 'punctumSize': 2}
+    query_list.append(query)
+    
+    n = 6
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'PSD' #String segment to search in a filename
+    target_fn_str = 'L113'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [], 'preIF_z': [], 'postIF': [target_name,conjugate_name], 'postIF_z': [1,2], 'punctumSize': 2}
+    query_list.append(query)    
+
+    n = 7
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'PSD' #String segment to search in a filename
+    target_fn_str = 'L113'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [], 'preIF_z': [], 'postIF': [target_name,conjugate_name], 'postIF_z': [1,2], 'punctumSize': 2}
+    query_list.append(query)   
+
+    n = 8
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'PSD' #String segment to search in a filename
+    target_fn_str = 'L114'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [target_name], 'preIF_z': [1], 'postIF': [conjugate_name], 'postIF_z': [2], 'punctumSize': 2}
+    query_list.append(query)   
+
+    n = 9
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'GAD2' #String segment to search in a filename
+    target_fn_str = 'L115'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [target_name,conjugate_name], 'preIF_z': [1,2], 'postIF': [], 'postIF_z': [], 'punctumSize': 2}
+    query_list.append(query)  
+
+    n = 10
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'PSD' #String segment to search in a filename
+    target_fn_str = 'L117'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [], 'preIF_z': [], 'postIF': [target_name,conjugate_name], 'postIF_z': [1,2], 'punctumSize': 2}
+    query_list.append(query) 
+
+    n = 11
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'GAD2' #String segment to search in a filename
+    target_fn_str = 'L118'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [target_name,conjugate_name], 'preIF_z': [1,2], 'postIF': [], 'postIF_z': [], 'punctumSize': 2}
+    query_list.append(query)
+
+    n = 12
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'GAD2' #String segment to search in a filename
+    target_fn_str = 'L120'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [conjugate_name], 'preIF_z': [2], 'postIF': [target_name], 'postIF_z': [1], 'punctumSize': 2}
+    query_list.append(query)
+
+    n = 13
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'SYNAPSIN' #String segment to search in a filename
+    target_fn_str = 'L121'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [target_name,conjugate_name], 'preIF_z': [1,2], 'postIF': [], 'postIF_z': [], 'punctumSize': 2}
+    query_list.append(query)
+
+    n = 14
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'SYNAPSIN' #String segment to search in a filename
+    target_fn_str = 'L122'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [target_name,conjugate_name], 'preIF_z': [1,2], 'postIF': [], 'postIF_z': [], 'punctumSize': 2}
+    query_list.append(query)
+
+    n = 15
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'SYNAPSIN' #String segment to search in a filename
+    target_fn_str = 'L122'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [target_name,conjugate_name], 'preIF_z': [1,2], 'postIF': [], 'postIF_z': [], 'punctumSize': 2}
+    query_list.append(query)
+
+    n = 16
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'SYNAPSIN' #String segment to search in a filename
+    target_fn_str = 'L124'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [target_name,conjugate_name], 'preIF_z': [1,2], 'postIF': [], 'postIF_z': [], 'punctumSize': 2}
+    query_list.append(query)
+
+    n = 17
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'SYNAPSIN' #String segment to search in a filename
+    target_fn_str = 'SP20'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [conjugate_name], 'preIF_z': [2], 'postIF': [target_name], 'postIF_z': [1], 'punctumSize': 2}
+    query_list.append(query)
+
+    n = 18
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'GAD2' #String segment to search in a filename
+    target_fn_str = 'SP20'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [conjugate_name], 'preIF_z': [2], 'postIF': [target_name], 'postIF_z': [1], 'punctumSize': 2}
+    query_list.append(query)
+
+    n = 19
+    folder_names.append('Control' + str(n)) # Collate 'dataset' names for excel sheet
+    reference_fn_str = 'PSD' #String segment to search in a filename
+    target_fn_str = 'SP20'
+    conjugate_name, target_name = aa.findFilenames(reference_fn_str, target_fn_str, filenames, n)
+    conjugate_filenames.append(conjugate_name)
+    target_filenames.append(target_name)
+    query = {'preIF': [target_name], 'preIF_z': [1], 'postIF': [conjugate_name], 'postIF_z': [2], 'punctumSize': 2}
+    query_list.append(query)
+
+
+    # Run all the queries
+    measure_list = aa.calculate_measure_lists(query_list, None, base_dir,
+                                        thresh, resolution, target_filenames)
+
+    df = aa.create_df(measure_list, folder_names, target_filenames, conjugate_filenames)
+    print(df)
+
+    return df
+    
+
+def main():
+    """ Run comparisons """
+
+    CPU_df = CPU()
+    
+    sheet_name = '20180417_AT_CPU'
+    fn = '20180417_AT_CPU.xlsx'
+    df_list = [CPU_df]
+    aa.write_dfs_to_excel(df_list, sheet_name, fn)
+    
+
+if __name__ == '__main__':
+    main()
