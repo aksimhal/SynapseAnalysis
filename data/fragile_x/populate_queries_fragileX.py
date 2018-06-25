34c3c3800191224313057fdbb0b08882b15469ff
Anish <aksimhal@gmail.com>
2018-06-25 15:27:00 -0400

    testing inhibitory queries

--- data/fragile_x/populate_queries_fragileX.py
+++ data/fragile_x/populate_queries_fragileX.py
@@ -110,6 +110,70 @@ def mouse_generic_queries(mouse_number):
     da.writeJSONFile(fn, data)
 
 
+def mouse_inhibitory_queries(mouse_number):
+    """ Inhibitory queries at different sizes
+
+    Parameters
+    -------------
+    mouse_number
+    """
+    listOfQueries = []
+    punctum_size = 2
+
+    # Query - 1 slice
+    preIF_str1 = str(mouse_number) + 'ss_Synap.tif'
+    preIF_str2 = str(mouse_number) + 'ss_GAD.tif'
+    preIF = [preIF_str1, preIF_str2]
+    preIF_z = [1, 1]
+    postIF_str1 = str(mouse_number) + 'ss_Geph.tif'
+    postIF = [postIF_str1]
+    postIF_z = [1]
+    query = {'preIF': preIF, 'preIF_z': preIF_z, 'postIF': postIF,
+             'postIF_z': postIF_z, 'punctumSize': punctum_size}
+    listOfQueries.append(query)
+
+    # Query
+    preIF_str1 = str(mouse_number) + 'ss_Synap.tif'
+    preIF_str2 = str(mouse_number) + 'ss_GAD.tif'
+    preIF = [preIF_str1, preIF_str2]
+    preIF_z = [2, 2]
+    postIF_str1 = str(mouse_number) + 'ss_Geph.tif'
+    postIF = [postIF_str1]
+    postIF_z = [1]
+    query = {'preIF': preIF, 'preIF_z': preIF_z, 'postIF': postIF,
+             'postIF_z': postIF_z, 'punctumSize': punctum_size}
+    listOfQueries.append(query)
+
+    # Query
+    preIF_str1 = str(mouse_number) + 'ss_Synap.tif'
+    preIF_str2 = str(mouse_number) + 'ss_GAD.tif'
+    preIF = [preIF_str1, preIF_str2]
+    preIF_z = [2, 2]
+    postIF_str1 = str(mouse_number) + 'ss_Geph.tif'
+    postIF = [postIF_str1]
+    postIF_z = [2]
+    query = {'preIF': preIF, 'preIF_z': preIF_z, 'postIF': postIF,
+             'postIF_z': postIF_z, 'punctumSize': punctum_size}
+    listOfQueries.append(query)
+
+    # Query
+    preIF_str1 = str(mouse_number) + 'ss_Synap.tif'
+    preIF_str2 = str(mouse_number) + 'ss_GAD.tif'
+    preIF = [preIF_str1, preIF_str2]
+    preIF_z = [3, 3]
+    postIF_str1 = str(mouse_number) + 'ss_Geph.tif'
+    postIF = [postIF_str1]
+    postIF_z = [3]
+    query = {'preIF': preIF, 'preIF_z': preIF_z, 'postIF': postIF,
+             'postIF_z': postIF_z, 'punctumSize': punctum_size}
+    listOfQueries.append(query)
+
+    data = {'listOfQueries': listOfQueries}
+    fn = '/Users/anish/Documents/Connectome/SynapseAnalysis/data/fragile_x/' + \
+        str(mouse_number) + 'ss_inhibitory_queries.json'
+    da.writeJSONFile(fn, data)
+
+
 def mouse_YFP_queries(mouse_number):
     """The generic list of queries expanded to include YFP for 2ss
     """
@@ -283,29 +347,32 @@ def mouse_astro_queries(mouse_number):
 
 def main():
 
-    mouse_generic_queries(2)
-    mouse_YFP_queries(2)
-    mouse_astro_queries(2)
+    # mouse_generic_queries(2)
+    # mouse_YFP_queries(2)
+    # mouse_astro_queries(2)
 
-    mouse_generic_queries(3)
-    mouse_YFP_queries(3)
-    mouse_astro_queries(3)
+    # mouse_generic_queries(3)
+    # mouse_YFP_queries(3)
+    # mouse_astro_queries(3)
 
-    mouse_generic_queries(4)
-    mouse_YFP_queries(4)
-    mouse_astro_queries(4)
+    # mouse_generic_queries(4)
+    # mouse_YFP_queries(4)
+    # mouse_astro_queries(4)
 
-    mouse_generic_queries(6)
-    mouse_YFP_queries(6)
-    mouse_astro_queries(6)
+    # mouse_generic_queries(6)
+    # mouse_YFP_queries(6)
+    # mouse_astro_queries(6)
 
-    mouse_generic_queries(5)
-    mouse_YFP_queries(5)
-    mouse_astro_queries(5)
+    # mouse_generic_queries(5)
+    # mouse_YFP_queries(5)
+    # mouse_astro_queries(5)
 
-    mouse_generic_queries(7)
-    mouse_YFP_queries(7)
-    mouse_astro_queries(7)
+    # mouse_generic_queries(7)
+    # mouse_YFP_queries(7)
+    # mouse_astro_queries(7)
+
+    for n in range(2, 8):
+        mouse_inhibitory_queries(n)
 
 
 if __name__ == '__main__':
--- data/fragile_x/run_fragX_L4.py
+++ data/fragile_x/run_fragX_L4.py
@@ -92,8 +92,87 @@ def run_queries_layer4():
     aa.write_dfs_to_excel(df_list, sheet_name, fn)
 
 
+def run_inhibitory_queries_layer4():
+    """
+    Run queries on L4 of the data 
+    F000 for 2ss, 3ss, 4ss, 5ss
+    F003 for 6ss, 7ss
+    """
+
+    mouse_id_list = [2, 3, 4, 5, 6, 7]
+    mouse_region_list = ['F000', 'F000', 'F000', 'F000', 'F003', 'F003']
+    output_foldername = 'results_inhibitory_layer4'
+    sheet_name = 'results_inhibitory_layer4'
+
+    resolution = {'res_xy_nm': 100, 'res_z_nm': 70}
+    thresh = 0.9
+
+    num_workers = mp.cpu_count() - 1
+    print(num_workers)
+    pool = mp.Pool(num_workers)
+
+    atet_inputs_list = []
+    result_list = []
+    foldernames = []
+    queryID = 0
+
+    for n, mouse_number in enumerate(mouse_id_list):
+
+        query_fn = str(mouse_number) + 'ss_inhibitory_queries.json'
+        listOfQueries = syn.loadQueriesJSON(query_fn)
+
+        data_location = '/Users/anish/Documents/yi_mice/' + \
+            str(mouse_number) + 'ss_stacks/'
+        mask_location_str = '/Users/anish/Documents/yi_mice/masks/' + \
+            str(mouse_number) + 'ss_mask.png'
+
+        hostname = socket.gethostname()
+        if hostname == 'Galicia':
+            data_location = '/data5TB/yi_mice/' + \
+                str(mouse_number) + 'ss_stacks'
+            mask_location_str = '/data5TB/yi_mice/L4masks/' + \
+                str(mouse_number) + 'ss_mask.png'
+            dapi_mask_str_base = '/data5TB/yi_mice/dapi-masks/' + \
+                str(mouse_number) + 'ss_stacks'
+
+        region_name = mouse_region_list[n]
+
+        data_region_location = os.path.join(data_location, region_name)
+        dapi_mask_str = os.path.join(dapi_mask_str_base, region_name)
+
+        for nQuery, query in enumerate(listOfQueries):
+            foldername = str(mouse_number) + 'ss-' + \
+                region_name + '-Q' + str(nQuery)
+            foldernames.append(foldername)
+            print(foldername)
+
+            atet_input = {'query': query, 'queryID': queryID, 'nQuery': nQuery, 'resolution': resolution,
+                          'data_region_location': data_region_location, 'data_location': data_location,
+                          'output_foldername': output_foldername, 'region_name': region_name,
+                          'mask_str': mask_location_str, 'dapi_mask_str': dapi_mask_str, 'mouse_number': mouse_number}
+            atet_inputs_list.append(atet_input)
+            queryID = queryID + 1
+
+    # Run processes
+    result_list = pool.map(sa.run_synapse_detection, atet_inputs_list)
+
+    pool.close()
+    pool.join()
+
+    print('Get process results from the output queue')
+    sorted_queryresult = sa.organize_result_lists(result_list)
+
+    mouse_df = sa.create_synapse_df(sorted_queryresult, foldernames)
+    print(mouse_df)
+
+    fn = sheet_name + '.xlsx'
+    df_list = [mouse_df]
+    aa.write_dfs_to_excel(df_list, sheet_name, fn)
+
+
 def main():
-    run_queries_layer4()
+    # run_queries_layer4()
+    run_inhibitory_queries_layer4()
 
 
 if __name__ == '__main__':
