0919910288f271c68cadea6e917f7eda68b0d520
Anish <aksimhal@gmail.com>
2019-01-10 21:53:58 -0500

    test of puncta queries

--- data/fragile_x/populate_queries_fragileX.py
+++ data/fragile_x/populate_queries_fragileX.py
@@ -6,6 +6,85 @@ import sys
 import json
 from at_synapse_detection import dataAccess as da
 
+def single_channel_queries(mouse_number):
+    """ Single channel queries
+
+    Parameters
+    -------------
+    mouse_number
+    """
+    listOfQueries = []
+    punctum_size = 2
+
+    for slice_span in range(1, 4):
+
+        # Query 0 - PSD-95
+        preIF = []
+        preIF_z = []
+        postIF_str = str(mouse_number) + 'ss_PSD.tif'
+        postIF = [postIF_str]
+        postIF_z = [slice_span]
+        query = {'preIF': preIF, 'preIF_z': preIF_z, 'postIF': postIF,
+                 'postIF_z': postIF_z, 'punctumSize': punctum_size}
+        listOfQueries.append(query)
+
+        # Query 1 - Synapsin
+        preIF_str1 = str(mouse_number) + 'ss_Synap.tif'
+        preIF = [preIF_str1]
+        preIF_z = [slice_span]
+        postIF = []
+        postIF_z = []
+        query = {'preIF': preIF, 'preIF_z': preIF_z, 'postIF': postIF,
+                 'postIF_z': postIF_z, 'punctumSize': punctum_size}
+        listOfQueries.append(query)
+
+        # Query 2 - VGluT1
+        preIF_str1 = str(mouse_number) + 'ss_VGluT1.tif'
+        preIF = [preIF_str1]
+        preIF_z = [slice_span]
+        postIF = []
+        postIF_z = []
+        query = {'preIF': preIF, 'preIF_z': preIF_z, 'postIF': postIF,
+                 'postIF_z': postIF_z, 'punctumSize': punctum_size}
+        listOfQueries.append(query)
+
+        # Query 3 - VGluT2
+        preIF_str1 = str(mouse_number) + 'ss_VGluT2.tif'
+        preIF = [preIF_str1]
+        preIF_z = [slice_span]
+        postIF = []
+        postIF_z = []
+        query = {'preIF': preIF, 'preIF_z': preIF_z, 'postIF': postIF,
+                 'postIF_z': postIF_z, 'punctumSize': punctum_size}
+        listOfQueries.append(query)
+
+        # Query 4 - Gephyrin
+        preIF = []
+        preIF_z = []
+        postIF_str = str(mouse_number) + 'ss_Geph.tif'
+        postIF = [postIF_str]
+        postIF_z = [slice_span]
+        query = {'preIF': preIF, 'preIF_z': preIF_z, 'postIF': postIF,
+                 'postIF_z': postIF_z, 'punctumSize': punctum_size}
+        listOfQueries.append(query)
+
+        # Query 5 - GAD
+        preIF_str1 = str(mouse_number) + 'ss_GAD.tif'
+        preIF = [preIF_str1]
+        preIF_z = [slice_span]
+        postIF = []
+        postIF_z = []
+        query = {'preIF': preIF, 'preIF_z': preIF_z, 'postIF': postIF,
+                 'postIF_z': postIF_z, 'punctumSize': punctum_size}
+        listOfQueries.append(query)
+
+
+    data = {'listOfQueries': listOfQueries}
+    fn = '/Users/anish/Documents/Connectome/SynapseAnalysis/data/fragile_x/queries/' + \
+        str(mouse_number) + 'ss_puncta_queries.json'
+    da.writeJSONFile(fn, data)
+
+
 
 def excitatory_queries(mouse_number):
     """ List of excitatory queries
@@ -393,17 +472,20 @@ def mouse_astroYFP_queries(mouse_number):
 def main():
     mice_list = [1, 2, 3, 4, 5, 6, 7, 22]
 
-    for n in mice_list:
-        excitatory_queries(n)
+    # for n in mice_list:
+    #     excitatory_queries(n)
+
+    # for n in mice_list:
+    #     inhibitory_queries(n)
+
+    # for n in mice_list:
+    #     excitatory_astro_queries(n)
+
+    # for n in mice_list:
+    #     inhibitory_astro_queries(n)
 
     for n in mice_list:
-        inhibitory_queries(n)
-
-    for n in mice_list:
-        excitatory_astro_queries(n)
-
-    for n in mice_list:
-        inhibitory_astro_queries(n)
+        single_channel_queries(n)
 
 
 if __name__ == '__main__':
