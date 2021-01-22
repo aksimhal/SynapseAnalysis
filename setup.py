18de768970a5e87ed8a37c52f82e3babd46719c5
Anish <aksimhal@users.noreply.github.com>
2021-01-21 21:00:05 -0500

    changed email address

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open('requirements.txt', 'r') as f:
     required = f.read().splitlines()
 
 setup(name='at_synapse_detection',
-      version='0.1.0',
+      version='0.1.1',
       description='synapse analysis for array tomography',
       author='Anish Simhal',
-      author_email='anish.simhal@duke.edu',
+      author_email='aksimhal@duke.edu',
       packages=find_packages(),
       url='https://github.com/aksimhal/SynapseAnalysis',
       install_requires=required)
